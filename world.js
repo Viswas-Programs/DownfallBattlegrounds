48e952dbd7bc6662a0196a358ab64534289b32d7
HeheBoi420 <115633223+Viswas-Programs@users.noreply.github.com>
2023-09-15 23:28:36 +0530

    Fixed broken removeLoot logic; added addPlayer and removePlayer functions

--- world.js
+++ world.js
@@ -11,11 +11,22 @@ export class World {
         this.loot.push(item)
     }
     removeLoot(discardLoot[]) {
-        var pendingLoot = this.loot;
+        var pendingLoot = []
         for (const loot of this.loot) {
-            if (!discardLoot.includes(loot.id)) pendingLoot.push(loot.id)
+            if (discardLoot.indexOf(loot.id)) pendingLoot.push(loot.id)
         }
         this.loot = pending;
+    }
+    addPlayer(id: string;) {
+        if (id.length > 4) return
+        this.players.push(id)
+    }
+    removePlayer(id: string) {
+        var pendingPlayer = [];
+        for (const player of this.players) {
+            if (this.players.indexOf(id)) pendingPlayer.push(player.id)
+        }
+        this.players = pendingPlayer;
     }
 
 }
